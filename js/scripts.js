8a0553da12ec452a71b362a98b62371320251bef
Calla Rudolph and Steve Spitz <steve@>
2017-06-01 09:37:09 -0700

    add radio buttons; color; date; name forms and accompanying java

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(function() {
   $("button").click(function() {
     var ffood = $("#fastfood").val();
-    alert(ffood);
+    var humanname = $("input#nombre").val();
+      alert(ffood + humanname);
     });
+  var flavor = $("input:radio[name=flavor]:checked").val();
+  var dob = $("#born").val();
+  var favoriteColor = $("#color").val();
 });
